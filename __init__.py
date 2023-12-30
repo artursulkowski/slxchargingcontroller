a6042ec2f7f9164588906cd6aeebce5f54d393de
ArturS <107952348+artursulkowski@users.noreply.github.com>
2023-12-30 19:41:20 +0100

    Tests - first test with monitoring of entity update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 """The Salix Charging Controller integration."""
 from __future__ import annotations
 
+import asyncio
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -25,15 +27,27 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Handle removal of an entry."""
+    #    _LOGGER.debug("Attempting to unload entities from the %s integration", DOMAIN)
 
-    # TODO - add once we have services
-    # if not hass.data[DOMAIN]:
-    #    async_unload_services(hass)
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(config_entry, platform)
+                for platform in PLATFORMS
+            ]
+        )
+    )
+    hass.data[DOMAIN].pop(config_entry.unique_id)
     return unload_ok
+
+
+# async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+#     """Unload a config entry."""
+#     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+#         hass.data[DOMAIN].pop(entry.entry_id)
+#     return unload_ok
 
 
 # TODO - add later for handling config compatiblity
