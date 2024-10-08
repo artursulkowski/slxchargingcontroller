83dd0b49663f7291917d78c762254fe21b7be190
ArturS <107952348+artursulkowski@users.noreply.github.com>
2024-10-08 19:54:19 +0200

    Added calculation of charged energy constraints

--- slxchargingplanner.py
+++ slxchargingplanner.py
@@ -10,7 +10,8 @@ For final checking we need a flat list of data (like cost).
 
 """
 
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, time, timezone
+from dataclasses import dataclass
 import logging
 from typing import Any
 
@@ -27,8 +28,40 @@ SOLAR_FORECAST_FILL_IN = 0
 ENERGY_COST_FILL_IN = None
 
 
+@dataclass
+class SLXSolarParameter:
+    bottom_cutoff: float
+    """in kW"""
+    upper_cutoff: float
+    """in kW"""
+    efficiency: float
+    """as a coefficient, e.g. 0.8. It cumulates with general charging efficiency!"""
+
+
+@dataclass
+class SLXChargingConfig:
+    charging_efficiency: float
+    """coefficient - e.g. 0.8"""
+
+    batery_capacity: float
+    """in kWh"""
+
+    bottom_buffer: float
+    """in kWh, minimum energy that should be left after planned daily drive"""
+
+    energy_consumption: float
+    """in kWh/km, car's energy consumption """
+
+    morning_start_utc: int
+
+
 class SLXChargingPlanner:
-    def __init__(self, hass: HomeAssistant) -> None:
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        solar_parameter: SLXSolarParameter | None = None,
+        charging_config: SLXChargingConfig | None = None,
+    ) -> None:
         """Creates empty planned object."""
 
         self.hass = hass
@@ -42,24 +75,49 @@ class SLXChargingPlanner:
 
         # Now the constraints! how much energy is needed in the battery
 
+        # PROBABLY TO REMOVE
         # here we use different approach - we provide the time by which given energy level needs to be achieved
         # we will calculate both datetime and the amount of energy mainly based on planned trips.
         # by mapping upcoming days into exact datetime we can also define easily which hour do we treat as the begin of the day(e.g. 6AM)
+        # energy in kWh
         self._energy_needed_in_bat: list[tuple[datetime, float]] = []
 
-        self._energy_in_bat_now: float = []
+        # energy in kWh - for the current hour
+        self._energy_in_bat_now: float | None = None
 
+        # Boundle of lists that stores information about energy requirements
+        self._energy_consumed_trimmed: list[tuple[datetime, float]] = []
+        """Energy to be consumed that specifc day. Datetime defines time in the morning when the car should be already charged. Trimmed - means that batter capacity is taken into consideration as the upper limit."""
+
+        self._cumulative_energy_min_max: list[tuple[datetime, float, float]] = []
+        """ """
+
+        # TO REMOVE
         # this will be used to define constraints in calculations!
         # takes into consideration any energy loses.
-        self._min_energy_to_be_added: list[tuple[datetime, float]] = []
+        # self._min_energy_to_be_added: list[tuple[datetime, float]] = []
 
         # that one can be probably ignored for now.
         # self._max_energy_to_be_added: list(tuple(datetime, float)) = []
 
         # parameters of solar forecast to real charging capabilities
-        self._solar_bottom_cutoff: float = 1.4
-        self._solar_upper_cutoff: float = 4.0
-        self._solar_coefficient: float = 0.7
+        if solar_parameter is not None:
+            self._solar_parameters = solar_parameter
+        else:
+            self._solar_parameters = SLXSolarParameter(
+                bottom_cutoff=1.4, upper_cutoff=4, efficiency=0.7
+            )
+
+        if charging_config is not None:
+            self._charging_config = charging_config
+        else:
+            self._charging_config = SLXChargingConfig(
+                charging_efficiency=0.8,
+                batery_capacity=64,
+                bottom_buffer=12.8,
+                energy_consumption=0.18,
+                morning_start_utc=3,
+            )
 
     def update_time_slots(self, currenttime: datetime):
         # dt_util.now()
@@ -79,22 +137,23 @@ class SLXChargingPlanner:
             ]
 
             # clean other related values.
-            self._solar_forecasts.clear()
-            self._energy_costs.clear()
+            ## I should fill it in!
+            self._solar_forecasts = [SOLAR_FORECAST_FILL_IN] * self._number_of_timeslots
+            self._energy_costs = [ENERGY_COST_FILL_IN] * self._number_of_timeslots
 
         else:
             # we already have timeslots defined. So now it is a matter of how much do we need to "shift"
             # Additionally we will need to add new timeslots
             # we are calculating of "how much to shift " and then we also shift solar forecast and energy cost
 
-            initial_len = len(self._time_slots)
-            if initial_len != self._number_of_timeslots:
+            ts_length = len(self._time_slots)
+            if ts_length != self._number_of_timeslots:
                 _LOGGER.error("Something is wrong!")
 
             self._time_slots = self._time_slots[index:]
             # fill in the remaining time slots
-            index_to_start = initial_len - index
-            for current_index in range(index_to_start, initial_len):
+            index_to_start = ts_length - index
+            for current_index in range(index_to_start, ts_length):
                 self._time_slots.append(new_start_time + timedelta(hours=current_index))
 
             # shift and fill in existing forecasts
@@ -138,7 +197,7 @@ class SLXChargingPlanner:
                 continue
             rounded = self._round_to_hour_utc(dt)
             index = self._find_index_in_timeslots(rounded)
-            if index is not -1:
+            if index != -1:
                 charging_power = self._solar_to_charging_power_kw(watts)
                 self._solar_forecasts[index] = charging_power
 
@@ -154,19 +213,60 @@ class SLXChargingPlanner:
                 continue
             rounded = self._round_to_hour_utc(dt)
             index = self._find_index_in_timeslots(rounded)
-            if index is not -1:
+            if index != -1:
                 self._energy_costs[index] = cost
 
-    def calculate_minimum_bat_energy(
-        self,
-        planned_distances: list[tuple[datetime, float]],
-        current_bat_energy_kwh: float,
-    ):
-        """Calculate minimum battery energy.
+    def calculate_energy_needed(self, planned_distances: list[tuple[date, float]]):
+        if self._energy_in_bat_now is None:
+            return
+        if len(self._time_slots) == 0:
+            return
 
-        goes rew
-        """
-        pass
+        self._energy_consumed_trimmed.clear()
+        self._cumulative_energy_min_max.clear()
+
+        for day, distance in planned_distances:
+            exact_time = datetime.combine(
+                day, time.min, tzinfo=timezone.utc
+            ) + timedelta(hours=self._charging_config.morning_start_utc)
+            if exact_time < self._time_slots[0]:
+                continue
+
+            energy_to_be_consumed_that_day = (
+                distance * self._charging_config.energy_consumption
+            )
+
+            energy_consumed_trimmed = min(
+                energy_to_be_consumed_that_day,
+                self._charging_config.batery_capacity
+                - self._charging_config.bottom_buffer,
+            )
+            self._energy_consumed_trimmed.append((exact_time, energy_consumed_trimmed))
+
+        min_energy_sum: float = 0
+        max_energy_sum: float = 0
+        for exact_time, energy_consumed_trimmed in self._energy_consumed_trimmed:
+            min_energy_sum += energy_consumed_trimmed
+
+            to_store_min_energy_cumulatively = (
+                min_energy_sum / self._charging_config.charging_efficiency
+                + self._charging_config.bottom_buffer
+                - self._energy_in_bat_now
+            )
+            to_store_max_energy_cumulatively = (
+                max_energy_sum / self._charging_config.charging_efficiency
+                + self._charging_config.batery_capacity
+                - self._energy_in_bat_now
+            )
+            max_energy_sum += energy_consumed_trimmed  # that is "one day later"
+
+            self._cumulative_energy_min_max.append(
+                (
+                    exact_time,
+                    to_store_min_energy_cumulatively,
+                    to_store_max_energy_cumulatively,
+                )
+            )
 
     def _isfullhour(self, dt: datetime) -> bool:
         return dt.minute == 0 and dt.second == 0 and dt.microsecond == 0
@@ -185,9 +285,9 @@ class SLXChargingPlanner:
             return -1
 
     def _solar_to_charging_power_kw(self, solar_forecast: float) -> float:
-        usable_kw = solar_forecast * self._solar_coefficient / 1000.0
-        if usable_kw < self._solar_bottom_cutoff:
+        usable_kw = solar_forecast * self._solar_parameters.efficiency / 1000.0
+        if usable_kw < self._solar_parameters.bottom_cutoff:
             usable_kw = 0
-        elif usable_kw > self._solar_upper_cutoff:
-            usable_kw = self._solar_upper_cutoff
+        elif usable_kw > self._solar_parameters.upper_cutoff:
+            usable_kw = self._solar_parameters.upper_cutoff
         return usable_kw
--- tests/test_slxchargingplanner.py
+++ tests/test_slxchargingplanner.py
@@ -1,11 +1,14 @@
 from datetime import date, datetime
 from math import isclose
 from unittest.mock import patch
+from typing import Tuple, Sequence
 
 from freezegun.api import FrozenDateTimeFactory, freeze_time
 
 from custom_components.slxchargingcontroller.slxchargingplanner import (
     SLXChargingPlanner,
+    SLXSolarParameter,
+    SLXChargingConfig,
 )
 from homeassistant.core import HomeAssistant
 # from homeassistant.helpers import storage
@@ -42,9 +45,166 @@ solar_prediction_test1 = {
     datetime.fromisoformat("2024-09-16T19:04:46+02:00"): 0,
 }
 
+solar_prediction_output = [
+    0.537,
+    0.979,
+    1.352,
+    1.659,
+    1.916,
+    2.06,
+    2.092,
+    1.979,
+    1.749,
+    1.421,
+    1.052,
+    0.656,
+    0.378,  # 19:00
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,  # 0:00
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,  # 7:00
+    0.82,  # 8:00
+    1.298,
+    1.693,
+    1.922,
+    2.133,
+    2.25,
+    2.089,
+    1.739,
+    1.291,
+    0.84,
+]
+
+
+energy_cost_test1 = {
+    datetime.fromisoformat("2024-09-15T06:00:00+02:00"): 0.500,
+    datetime.fromisoformat("2024-09-15T07:00:00+02:00"): 0.650,
+    datetime.fromisoformat("2024-09-15T08:00:00+02:00"): 0.900,
+    datetime.fromisoformat("2024-09-15T09:00:00+02:00"): 1.200,
+    datetime.fromisoformat("2024-09-15T10:00:00+02:00"): 0.900,
+    datetime.fromisoformat("2024-09-15T11:00:00+02:00"): 0.850,
+    datetime.fromisoformat("2024-09-15T12:00:00+02:00"): 0.450,
+    datetime.fromisoformat("2024-09-15T13:00:00+02:00"): 0.300,
+    datetime.fromisoformat("2024-09-15T14:00:00+02:00"): 0.200,
+    datetime.fromisoformat("2024-09-15T15:00:00+02:00"): 0.450,
+    datetime.fromisoformat("2024-09-15T16:00:00+02:00"): 0.750,
+    datetime.fromisoformat("2024-09-15T17:00:00+02:00"): 1.100,
+    datetime.fromisoformat("2024-09-15T18:00:00+02:00"): 1.450,
+    datetime.fromisoformat("2024-09-15T19:00:00+02:00"): 1.550,
+    datetime.fromisoformat("2024-09-15T20:00:00+02:00"): 1.400,
+    datetime.fromisoformat("2024-09-15T21:00:00+02:00"): 0.900,
+    datetime.fromisoformat("2024-09-15T22:00:00+02:00"): 0.700,
+    datetime.fromisoformat("2024-09-15T23:00:00+02:00"): 0.600,
+    datetime.fromisoformat("2024-09-16T00:00:00+02:00"): 0.300,
+    datetime.fromisoformat("2024-09-16T01:00:00+02:00"): -0.010,
+    datetime.fromisoformat("2024-09-16T02:00:00+02:00"): -0.050,
+    datetime.fromisoformat("2024-09-16T03:00:00+02:00"): -0.150,
+    datetime.fromisoformat("2024-09-16T04:00:00+02:00"): 0.300,
+    datetime.fromisoformat("2024-09-16T05:00:00+02:00"): 0.400,
+    datetime.fromisoformat("2024-09-16T06:00:00+02:00"): 0.600,
+    datetime.fromisoformat("2024-09-16T07:00:00+02:00"): 0.900,
+    datetime.fromisoformat("2024-09-16T08:00:00+02:00"): 1.300,
+    datetime.fromisoformat("2024-09-16T09:00:00+02:00"): 0.900,
+    datetime.fromisoformat("2024-09-16T10:00:00+02:00"): 0.700,
+    datetime.fromisoformat("2024-09-16T11:00:00+02:00"): 0.700,
+    datetime.fromisoformat("2024-09-16T12:00:00+02:00"): 0.500,
+    datetime.fromisoformat("2024-09-16T13:00:00+02:00"): 0.450,
+    datetime.fromisoformat("2024-09-16T14:00:00+02:00"): 0.350,
+    datetime.fromisoformat("2024-09-16T15:00:00+02:00"): 0.400,
+    datetime.fromisoformat("2024-09-16T16:00:00+02:00"): 0.650,
+    datetime.fromisoformat("2024-09-16T17:00:00+02:00"): 0.850,
+    datetime.fromisoformat("2024-09-16T18:00:00+02:00"): 0.900,
+}
+
+
+energy_cost_output = [
+    0.650,
+    0.900,
+    1.200,
+    0.900,
+    0.850,
+    0.450,
+    0.300,
+    0.200,
+    0.450,
+    0.750,
+    1.100,
+    1.450,
+    1.550,
+    1.400,
+    0.900,
+    0.700,
+    0.600,
+    0.300,
+    -0.010,
+    -0.050,
+    -0.150,
+    0.300,
+    0.400,
+    0.600,
+    0.900,
+    1.300,
+    0.900,
+    0.700,
+    0.700,
+    0.500,
+    0.450,
+    0.350,
+    0.400,
+    0.650,
+    0.850,
+    0.900,
+]
+
+
+def compare_timeslots_float(
+    referenced_timeslots: list[float],
+    calculated_timeslots: list[float],
+    reference_shorter=True,
+):
+    if reference_shorter is False:  # allow differnt
+        assert len(referenced_timeslots) == len(calculated_timeslots)
+
+    index = 0
+    for referenced_entry, calculated_entry in zip(
+        referenced_timeslots, calculated_timeslots, strict=False
+    ):
+        assert isclose(
+            referenced_entry, calculated_entry
+        ), f"Not the same value, reference={referenced_entry}, calculated{calculated_entry}, index={index}"
+        index += 1
+
+
+def compare_datetime_float(
+    referenced_entries: list[(datetime, Sequence[float])],
+    calculated_entries: list[(datetime, Sequence[float])],
+):
+    assert len(referenced_entries) == len(calculated_entries)
+    for referenced_entry, calculated_entry in zip(
+        referenced_entries, calculated_entries, strict=True
+    ):
+        assert (
+            referenced_entry[0] == calculated_entry[0]
+        ), f"Not the same date, reference={referenced_entry[0]}, calculated{calculated_entry[0]}"
+
+        for index in range(1, len(referenced_entry)):
+            assert isclose(
+                referenced_entry[index], calculated_entry[index]
+            ), f"Values are different for {referenced_entry[0]}: ref_value={referenced_entry[index]}, calculated_value={calculated_entry[index]}"
+
 
 def test_fill_in_solar(hass: HomeAssistant) -> None:
-    charging_planner = SLXChargingPlanner(hass)
+    charging_planner = SLXChargingPlanner(
+        hass, SLXSolarParameter(bottom_cutoff=0, upper_cutoff=20, efficiency=1.0)
+    )
 
     charging_planner.update_time_slots(
         datetime.fromisoformat("2024-09-15T07:00:00+02:00")
@@ -52,3 +212,123 @@ def test_fill_in_solar(hass: HomeAssistant) -> None:
     charging_planner.fill_in_solar_forecast(solar_prediction_test1)
 
     assert len(charging_planner._time_slots) == len(charging_planner._solar_forecasts)
+    compare_timeslots_float(solar_prediction_output, charging_planner._solar_forecasts)
+
+    charging_planner.update_time_slots(
+        datetime.fromisoformat("2024-09-15T09:00:00+02:00")
+    )
+    # slots must be filled in
+    assert len(charging_planner._time_slots) == len(charging_planner._solar_forecasts)
+    assert len(charging_planner._time_slots) == len(charging_planner._energy_costs)
+
+    # we are checking if slots are correctly shifted by two slots
+    compare_timeslots_float(
+        solar_prediction_output[2:], charging_planner._solar_forecasts
+    )
+
+
+def test_fill_in_energy(hass: HomeAssistant) -> None:
+    ## we are going to check if the slots are shifted according to timeslot. We are putting the neutral SLXSolarParameters.
+    charging_planner = SLXChargingPlanner(
+        hass, SLXSolarParameter(bottom_cutoff=0, upper_cutoff=20, efficiency=1.0)
+    )
+
+    charging_planner.update_time_slots(
+        datetime.fromisoformat("2024-09-15T07:00:00+02:00")
+    )
+    charging_planner.fill_in_energy_cost(energy_cost_test1)
+    assert len(charging_planner._time_slots) == len(charging_planner._energy_costs)
+    compare_timeslots_float(energy_cost_output, charging_planner._energy_costs)
+
+    charging_planner.update_time_slots(
+        datetime.fromisoformat("2024-09-15T09:00:00+02:00")
+    )
+    assert len(charging_planner._time_slots) == len(charging_planner._energy_costs)
+    compare_timeslots_float(energy_cost_output[2:], charging_planner._energy_costs)
+
+    # go beyond available data
+    charging_planner.update_time_slots(
+        datetime.fromisoformat("2024-09-16T19:00:00+02:00")
+    )
+    # length should be kept the same
+    assert len(charging_planner._time_slots) == len(charging_planner._energy_costs)
+    # all values should be None
+
+    for item in charging_planner._energy_costs:
+        assert item is None
+
+
+planned_distances_test1 = [
+    (date.fromisoformat("2024-09-16"), 20),
+    (date.fromisoformat("2024-09-17"), 100),
+    (date.fromisoformat("2024-09-18"), 50),
+    (date.fromisoformat("2024-09-19"), 600),
+    (date.fromisoformat("2024-09-20"), 30),
+    (date.fromisoformat("2024-09-21"), 0),
+    (date.fromisoformat("2024-09-22"), 30),
+]
+
+
+calculate_min_bat_energy_test1_params = {
+    "planned_distances": planned_distances_test1,
+    "energy_consumption": 0.18,
+    "bottom_buffer": 10,
+    "morning_start_utc": 3,
+}
+
+charging_config_test1 = SLXChargingConfig(
+    charging_efficiency=0.8,
+    batery_capacity=64,
+    bottom_buffer=8,
+    energy_consumption=0.18,
+    morning_start_utc=3,
+)
+
+output_energy_consumed_trimmed_test1 = [
+    (datetime.fromisoformat("2024-09-16T03:00:00+00:00"), 3.6),
+    (datetime.fromisoformat("2024-09-17T03:00:00+00:00"), 18),
+    (datetime.fromisoformat("2024-09-18T03:00:00+00:00"), 9),
+    (datetime.fromisoformat("2024-09-19T03:00:00+00:00"), 56),
+    (datetime.fromisoformat("2024-09-20T03:00:00+00:00"), 5.4),
+    (datetime.fromisoformat("2024-09-21T03:00:00+00:00"), 0),
+    (datetime.fromisoformat("2024-09-22T03:00:00+00:00"), 5.4),
+]
+
+output_energy_cumulative_min_max1 = [
+    (datetime.fromisoformat("2024-09-16T03:00:00+00:00"), 2.5, 54),
+    (datetime.fromisoformat("2024-09-17T03:00:00+00:00"), 25, 58.5),
+    (datetime.fromisoformat("2024-09-18T03:00:00+00:00"), 36.25, 81),
+    (datetime.fromisoformat("2024-09-19T03:00:00+00:00"), 106.25, 92.25),
+    (datetime.fromisoformat("2024-09-20T03:00:00+00:00"), 113.0, 162.25),
+    (datetime.fromisoformat("2024-09-21T03:00:00+00:00"), 113.0, 169.0),
+    (datetime.fromisoformat("2024-09-22T03:00:00+00:00"), 119.75, 169.0),
+]
+
+calculated_energy_needed_test1 = [
+    (datetime.fromisoformat("2024-09-16T03:00:00+00:00"), 13.6),
+    (datetime.fromisoformat("2024-09-17T03:00:00+00:00"), 28),
+    (datetime.fromisoformat("2024-09-18T03:00:00+00:00"), 19),
+]
+
+
+def test_calculate_energy_needed(hass: HomeAssistant) -> None:
+    charging_planner = SLXChargingPlanner(
+        hass,
+        SLXSolarParameter(bottom_cutoff=0, upper_cutoff=20, efficiency=1.0),
+        charging_config_test1,
+    )
+
+    charging_planner.update_time_slots(
+        datetime.fromisoformat("2024-09-15T07:00:00+02:00")
+    )
+
+    charging_planner._energy_in_bat_now = 10
+    charging_planner.calculate_energy_needed(planned_distances_test1)
+
+    compare_datetime_float(
+        output_energy_consumed_trimmed_test1, charging_planner._energy_consumed_trimmed
+    )
+
+    compare_datetime_float(
+        output_energy_cumulative_min_max1, charging_planner._cumulative_energy_min_max
+    )
